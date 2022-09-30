f2074e4675418841a4c89e8867ecec26ef236f1f
Nadezhda Yunker <nadezhdamamina@rambler.ru>
2022-09-30 12:15:59 +0200

    done

---
+++ 06_bundlers/test/dist/js/main.js
@@ -0,0 +1,10 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!************************!*\
+  !*** ./src/js/main.js ***!
+  \************************/
+const obj = {};
+const prop = obj === null || obj === void 0 ? void 0 : obj.prop;
+/******/ })()
+;
+//# sourceMappingURL=main.js.map
