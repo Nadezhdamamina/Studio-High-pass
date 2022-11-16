54937f795590cc7a06f49d555330737b0e9a348b
Nadezhda Yunker <nadezhdamamina@rambler.ru>
2022-11-16 14:35:18 +0200

    ignore

--- css-grid/src/js/_components.js
+++
@@ -1,7 +0,0 @@
-console.log('components');
-import './components/toggle-form';
-import './components/dynamic_adapt.js';
-import './components/map.js';
-
-// import * as flsFunctions from "./components/burger";
-// flsFunctions.menuInit();
